ccc7f8423d5e13398d0f454f392c6da333a38c00
Nanthana02 <71430491+Nanthana02@users.noreply.github.com>
2023-08-09 13:39:04 +0700

    add file

--- 1-3-hello-world-task.py
+++ 1-3-hello-world-task.py
@@ -8,11 +8,11 @@ async def print_after(message, delay):
 
 async def main():
    # Start coroutine twice (hopefully they start!)
-   first_awaitable = asyncio.create_task(print_after("world!", 2))
    second_awaitable = asyncio.create_task(print_after("Hello", 1))
+   first_awaitable = asyncio.create_task(print_after("world!", 2))
    # Wait for coroutines to finish
-   await first_awaitable
    await second_awaitable
+   await first_awaitable
 
 asyncio.run(main())
 
