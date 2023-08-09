a38bde24b0b7434c98fbc66cbecb04cfd44a3c5a
Nanthana02 <71430491+Nanthana02@users.noreply.github.com>
2023-08-09 22:20:05 +0700

    add file

--- 4-1-cooking.py
+++ 4-1-cooking.py
@@ -1,6 +1,5 @@
 import asyncio
 import time
-
 class Coffee:
     pass
 class Egg:
@@ -9,25 +8,35 @@ class Toast:
     pass
 
 def PourCoffee():
-    
+    print(f"{time.ctime()} - Pouring coffee")
+    return Coffee()
 
 async def ApplyButter():
-    
-    return
+    print(f"{time.ctime()} - Spreading butter on toast")
+    await asyncio.sleep(1)
+
   
 async def FryEggsAsync(howMany):
-    
+    print(f"{time.ctime()} - Heat pan to fry eggs")
+    await asyncio.sleep(3)
+    print(f"{time.ctime()} - Frying",howMany,"eggs")
+    await asyncio.sleep(3)
+    print(f"{time.ctime()} - Eggs are ready")
     return Egg()
 
 async def ToastAsync(slices):
     for slice in range(slices):
-      
+      print(f"{time.ctime()} - Toasting bread", slice + 1)
+      await asyncio.sleep(3)
+      print(f"{time.ctime()} - Bread", slice + 1, "toasted")
+      await ApplyButter()
+      print (f"{time.ctime()} - Toast", slice + 1, "ready")
     return Toast()
 
 async def main():
     cup = PourCoffee()
-    print(f"{time.ctime()} - Coffee is ready")
-    await 
+    print(f"{time.ctime()}- Coffee is ready")
+    await asyncio.gather(FryEggsAsync(2),ToastAsync(2))
     
 if __name__ == "__main__":
     import time
@@ -35,3 +44,19 @@ if __name__ == "__main__":
     asyncio.run(main())
     elapsed = time.perf_counter() - s
     print(f"{time.ctime()} - Breakfast cooked in",elapsed,"seconds.")
+ 
+
+# Wed Aug  9 22:17:27 2023 - Pouring coffee
+# Wed Aug  9 22:17:27 2023- Coffee is ready
+# Wed Aug  9 22:17:27 2023 - Heat pan to fry eggs
+# Wed Aug  9 22:17:27 2023 - Toasting bread 1
+# Wed Aug  9 22:17:30 2023 - Frying 2 eggs
+# Wed Aug  9 22:17:30 2023 - Bread 1 toasted
+# Wed Aug  9 22:17:30 2023 - Spreading butter on toast
+# Wed Aug  9 22:17:31 2023 - Toast 1 ready
+# Wed Aug  9 22:17:31 2023 - Toasting bread 2
+# Wed Aug  9 22:17:33 2023 - Eggs are ready
+# Wed Aug  9 22:17:34 2023 - Bread 2 toasted
+# Wed Aug  9 22:17:34 2023 - Spreading butter on toast
+# Wed Aug  9 22:17:35 2023 - Toast 2 ready
+# Wed Aug  9 22:17:35 2023 - Breakfast cooked in 8.054980600019917 seconds.
