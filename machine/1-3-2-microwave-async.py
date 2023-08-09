d0f147cbac9ce10b5103177fed95545440990b84
Nanthana02 <71430491+Nanthana02@users.noreply.github.com>
2023-08-09 14:49:13 +0700

    add file

--- machine/1-3-2-microwave-async.py
+++ machine/1-3-2-microwave-async.py
@@ -21,14 +21,27 @@ import asyncio
 import time
 
 async def cook(food, t):
-    
+    print(f'{time.ctime()} - Microwave ({food}): Cooking {t} seconds...')
+    await asyncio.sleep(t)
+    print(f'{time.ctime()} - Microwave ({food}): Finished cooking')
+    return f'{food} is completed'
 
 async def main():
-    
-
+    coros = [cook('Rice', 5), cook('Noodle', 3), cook('Curry', 1)]
+    results = await asyncio.wait(coros, return_when='FIRST_COMPLETED')
+    print(f'Completed task: {len(results[0])}')
+    for completed_task in results[0]:
+        print(f' - {completed_task.result()}')
+    print(f'Uncompleted task: {len(results[1])}')
 
 if __name__ == '__main__':
     t1 = time.time()
     asyncio.run(main())
     t2 = time.time() - t1
     print(f'Executed in {t2:0.2f} seconds.')
+
+
+# Wed Aug  9 14:46:43 2023 - Microwave (Rice): Cooking 5 seconds...
+# Wed Aug  9 14:46:43 2023 - Microwave (Curry): Cooking 1 seconds...
+# Wed Aug  9 14:46:43 2023 - Microwave (Noodle): Cooking 3 seconds...
+# Wed Aug  9 14:46:44 2023 - Microwave (Curry): Finished cooking
